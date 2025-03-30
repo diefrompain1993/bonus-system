85ee58350194c2edd2949bdf7a20845e13ffc974
Aleksei <aleksei.bat18@mail.ru>
2025-03-30 23:58:15 +0300

    Первый комт

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'; // ← вот это обязательно
+import { defineConfig } from 'vite'; // ← это обязательно
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    cors: true,
+    host: '0.0.0.0', // делаем сервер доступным на всех интерфейсах
+    port: 5173, // устанавливаем порт
+    strictPort: true, // если порт занят, сервер не запустится
+    cors: true, // разрешаем CORS
     headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
+      'Access-Control-Allow-Origin': '*', // разрешаем все источники
+    },
+    open: '/bonus', // автоматически открывается /bonus при старте
   }
 });
